e0daf3a49917508b229ffa9f6a7c3af048376ddb
Diego Martinez <dmartinez-2023307@kinal.edu.gt>
2026-09-17 10:55:38 -0600

    Feature: Añadir vista de registro

---
+++ src/edu/eduark/bizarre/fabrica/flowtech/MainApp.java
@@ -0,0 +1,12 @@
+
+package edu.eduark.bizarre.fabrica.flowtech;
+
+
+public class MainApp {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/edu/eduark/bizarre/fabrica/flowtech/controller/RegistroController.java
@@ -0,0 +1,17 @@
+
+package edu.eduark.bizarre.fabrica.flowtech.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+
+public class RegistroController implements Initializable {
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
