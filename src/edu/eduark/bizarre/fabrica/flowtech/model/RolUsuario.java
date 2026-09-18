990fcf910d33319be6583158c2d23aa9ec25273d
tonygt4 <antonygh509@gmail.com>
2026-09-18 08:39:20 -0600

    Implementacion de dashboard de roles

---
+++ src/edu/eduark/bizarre/fabrica/flowtech/model/RolUsuario.java
@@ -0,0 +1,11 @@
+package edu.eduark.bizarre.fabrica.flowtech.model;
+
+/**
+ * Roles de usuario del sistema FlowTech.
+ * Determina qué módulos del dashboard puede ver cada quien.
+ */
+public enum RolUsuario {
+    CLIENTE,
+    EMPLEADO,
+    ADMINISTRADOR
+}
--- src/edu/eduark/bizarre/fabrica/flowtech/utils/SceneManager.java
+++ src/edu/eduark/bizarre/fabrica/flowtech/utils/SceneManager.java
@@ -7,15 +7,13 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class SceneManager {
-    
 
     private static SceneManager instance;
-    private Stage primaryStage;
 
+    private Stage primaryStage;
 
     private SceneManager() {
     }
-
 
     public static SceneManager getInstance() {
         if (instance == null) {
@@ -24,29 +22,47 @@ public class SceneManager {
         return instance;
     }
 
-
     public void setPrimaryStage(Stage stage) {
         this.primaryStage = stage;
     }
 
-
     public void changeScene(String fxmlPath, String title) {
         try {
-
             FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
             Parent root = loader.load();
-            
 
             Scene scene = new Scene(root);
-            
 
             primaryStage.setTitle(title);
             primaryStage.setScene(scene);
             primaryStage.show();
-            
         } catch (IOException e) {
             System.out.println("Error al cargar la vista: " + fxmlPath);
             e.printStackTrace();
+        }
+    }
+
+    /**
+     * Igual que changeScene, pero devuelve el controller de la vista cargada
+     * para poder pasarle datos justo después (por ejemplo, el usuario y el
+     * rol que acaban de iniciar sesión, hacia DashboardController).
+     */
+    public <T> T changeSceneAndGetController(String fxmlPath, String title) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root);
+
+            primaryStage.setTitle(title);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+            return loader.getController();
+        } catch (IOException e) {
+            System.out.println("Error al cargar la vista: " + fxmlPath);
+            e.printStackTrace();
+            return null;
         }
     }
 }
