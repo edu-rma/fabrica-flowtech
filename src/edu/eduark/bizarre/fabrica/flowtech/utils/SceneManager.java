369797e3f2b9612f10074a1dc4fc07aa9a6e21e7
tonygt4 <antonygh509@gmail.com>
2026-09-25 08:00:52 -0600

    alertasjava

--- src/edu/eduark/bizarre/fabrica/flowtech/utils/SceneManager.java
+++ src/edu/eduark/bizarre/fabrica/flowtech/utils/SceneManager.java
@@ -1,9 +1,12 @@
 package edu.eduark.bizarre.fabrica.flowtech.utils;
 
+import edu.eduark.bizarre.fabrica.flowtech.controller.cliente.ClienteDashboardController;
+import edu.eduark.bizarre.fabrica.flowtech.model.RolUsuario;
 import java.io.IOException;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Dialog;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -93,5 +96,59 @@ public class SceneManager {
             e.printStackTrace();
             return null;
         }
+
+    }
+    
+        public void showAlert(Alert.AlertType type, String title, String header, String content) {
+        Alert alert = new Alert(type);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+        if (primaryStage != null) {
+            alert.initOwner(primaryStage);
+        }
+        alert.showAndWait();
+    }
+
+    public void showWarning(String title, String message) {
+        showAlert(Alert.AlertType.WARNING, title, null, message);
+    }
+
+    public void showError(String title, String message) {
+        showAlert(Alert.AlertType.ERROR, title, null, message);
+    }
+
+    public void showInformation(String title, String message) {
+        showAlert(Alert.AlertType.INFORMATION, title, null, message);
+    }
+
+ 
+    public void redirigirPorRol(int idUsuario, int idRol, String nombreUsuario) {
+        switch (idRol) {
+            case 1:
+                changeScene(
+                    "/edu/eduark/bizarre/fabrica/flowtech/resources/view/dashboard-admin.fxml", 
+                    "FlowTech - Dashboard Admin"
+                );
+                break;
+            case 2:
+                changeScene(
+                    "/edu/eduark/bizarre/fabrica/flowtech/resources/view/dashboard-empleado.fxml", 
+                    "FlowTech - Dashboard Empleado"
+                );
+                break;
+            case 3:
+                ClienteDashboardController controlador = changeSceneAndGetController(
+                    "/edu/eduark/bizarre/fabrica/flowtech/resources/view/dashboard-cliente-view.fxml", 
+                    "FlowTech - Dashboard Cliente"
+                );
+                if (controlador != null) {
+                    controlador.configurarUsuario(idUsuario, nombreUsuario, RolUsuario.CLIENTE);
+                }
+                break;
+            default:
+                showError("Rol Desconocido", "El rol asignado a este usuario no es reconocido por el sistema.");
+        }
     }
 }
+
